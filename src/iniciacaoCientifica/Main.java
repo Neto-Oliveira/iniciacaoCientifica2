4a606714de44899d82e071568150c17ebbaddd6a
Administrador <Administrador@NetooLr>
2025-01-19 09:46:51 -0300

    finalizando a classe cadastrar jogadores

--- src/iniciacaoCientifica/Main.java
+++ src/iniciacaoCientifica/Main.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
 //18/01/2025 passar para POO amanhã
 public class Main {
 	public static void main(String args[]) {
-		cadastrojogadores jogador1 = new cadastrojogadores();
+		
+		cadastrarJogadores jogador1 = new cadastrarJogadores(null, null, 0, 0, 0);
+		jogador1.cadastrar();
+		System.out.println("Nome:"+ jogador1.nome +""
+				+ "\nJogo: " + jogador1.nomeJogo
+				+ "\nId:" + jogador1.id
+				+ "\nBai inicial:" +jogador1.pontuacaoInicialBAI
+				+ "\nBai final:"+jogador1.pontuacaoFinalBAI);
 	}
 }
